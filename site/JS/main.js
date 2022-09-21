89744579ef482caf7dd2ddf7c6d84b0e09d2d2b5
Ben Keel <bennkeel@gmail.com>
2022-09-21 13:02:17 -0400

    Attempts at converting GeoJson to leaflet forms

--- site/JS/main.js
+++ site/JS/main.js
@@ -1,6 +1,6 @@
 import schools from '../data/schools.js'
 
-let schoolMap = L.map('school-map').setView([39.9, -72.3], 10);
+let schoolMap = L.map('school-map').setView([39.95, -75.2], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -9,3 +9,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //Expose variables to the global scope
 window.schools = schools;
+
+
+function makeSchoolFeature(d) {
+    let thisSchool = schools[d];
+    thisSchool.geom.coordinates = L.GeoJson.coordsToLatLngs(thisSchool.geom.coordinates)
+    console.log(thisSchool.geom.coordinates)
+    return thisSchool
+}
+
+/**for (let i = 0; i < schools.length; i++){
+   
+    let focus = makeSchoolFeature(i)
+    let circle = L.circle(thisSchool.geom.coordinates)
+
+    
+}**/
+makeSchoolFeature(1);
+
