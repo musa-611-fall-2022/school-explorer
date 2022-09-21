74f59eb176826e760b9e220198ce96cc87d77763
Jonathon Sun <Jonathonsun03@gmail.com>
2022-09-21 13:57:31 -0400

    Updating fork

--- site/js/CleanSchools.js
+++ site/js/CleanSchools.js
@@ -1,3 +1,5 @@
 function makeSchoolFeature(){
     
 }
+
+export{makeSchoolFeature}
--- site/js/main.js
+++ site/js/main.js
@@ -2,6 +2,8 @@
 //.. indicates from where I am look one folder up and then start going down again.
 import schools from '../data/schools.js';
 
+import{makeSchoolFeature} from "./CleanSchools.js"
+
 let schoolMap = L.map('school-map').setView([40.0021623,-75.3975295], 10); //L. usually refers to the leaflet library. You can also type Leaflet.map. This function binds a map to a container. We are creating a school-map. This is lat lon, the third arguement is the zoom. Which is a lens of 10 for this case. 
 let marker = L.marker([39.9885051,-75.185172]).addTo(schoolMap);
 
