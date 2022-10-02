f1b5913e46de30b78012c9d21a0ce21e5f587a6d
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-02 15:50:02 -0400

    Trying to figure out what schools is not uploading

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,13 @@
-import schools from '../data/schools.js';
+import schools from "../data/schools.js"
+import catchment from '../data/catchments.js';
 
 let schoolMap = L.map('school-map').setView([40.0, -75.11], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '© OpenStreetMap' }).addTo(schoolMap);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+    maxZoom: 19, 
+    attribution: '© OpenStreetMap',
+ }).addTo(schoolMap);
 
 // Expose variables to the global scope
 window.schools = schools;
+window.schools = catchment;
