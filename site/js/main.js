ffbeb12f71ba31c6de37ea0bf1d5d5ecacdfd48e
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-02 17:34:29 -0400

    Made function to pull each select variables for each stop

--- site/js/main.js
+++ site/js/main.js
@@ -7,7 +7,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19, 
     attribution: '© OpenStreetMap',
  }).addTo(schoolMap);
+ 
+
+// Function to pull geomtery
+// function pulls select features from each school
+function makeSchoolFeature(school){
+    return{
+        'type': 'Feature',
+        'id': school["sdp_id"],
+        'properties':{
+            "school_name": school["name"],
+            "Email": school["FACE Liason Email"],
+            "Phone Number": school["FACE Liasion Phone Number"],
+            "Admission type": school["Admission Type"],
+            "Grades Served": school["Current Grade Span Served"],
+        },
+        "geometry":stop["geom"]
+    }
+}
 
 // Expose variables to the global scope
+window.makeSchoolFeature = makeSchoolFeature;
 window.schools = schools;
-window.schools = catchment;
+window.catchment = catchment;
