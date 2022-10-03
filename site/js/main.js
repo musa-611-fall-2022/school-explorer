d8fc4f97f757d4adcf8918bfe706c25e8d6336c2
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-03 19:17:25 -0400

    Text input counter works

--- site/js/main.js
+++ site/js/main.js
@@ -4,7 +4,11 @@ import { initializeSchoolMap, showSchoolsOnMap} from './schools-maps.js';
 let schoolMap = initializeSchoolMap();
 showSchoolsOnMap(schools, schoolMap)
 
+//All is for all the check boxes
 let schoolCheckboxes = document.querySelectorAll('.grade-checkbox');
+
+//No all because we only have one text box
+let schoolNameInput = document.querySelector('#school-name-input');
 
 for (const cb of schoolCheckboxes) {
     cb.addEventListener('change', (evt) => {
@@ -13,9 +17,15 @@ for (const cb of schoolCheckboxes) {
     });
 }
 
+//For when a change is made
+schoolNameInput.addEventListener('change',() => {
+    console.log('text changed: ' + schoolNameInput.value);
+});
 
-
-
+//For when content is inputted
+schoolNameInput.addEventListener('input',() => {
+    console.log('text changed: ' + schoolNameInput.value);
+});
 
 // Expose variables to the global scope
 window.schools = schools;
