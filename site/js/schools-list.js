4c6e8eb8fb25c304a95df68f0d872bd276daee60
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-07 10:21:32 -0400

    Still haven't figured out why my list isn't working. Will send for help

--- site/js/schools-list.js
+++ site/js/schools-list.js
@@ -1,6 +1,7 @@
 import { htmlToElement } from './template-tools.js';
 
 function showSchoolsInList(schoolsToShow, schoolList) {
+    console.log(schoolList);
     schoolList.innerHTML = '';
 
     for (const school of schoolsToShow) {
@@ -8,7 +9,7 @@ function showSchoolsInList(schoolsToShow, schoolList) {
             <li class="school-list-item">${school['name']}</li>
         `;
         const li = htmlToElement(html);
-        schoolList.append(li);
+        schoolList.appendChild(li);
     }
 }
 
