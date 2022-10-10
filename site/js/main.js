9e091af28d89cb21180aa2b18335ee2aa3d929d7
Yesao <673392438@qq.com>
2022-10-10 11:24:04 -0400

    Finished All the Tasks. Shengao Yi

--- site/js/main.js
+++ site/js/main.js
@@ -4,10 +4,18 @@ import { showSchoolsInList } from './school-list.js';
 
 let schoolMap = L.map('school-map').setView([40.0055537, -75.120751], 11.5);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap',
+const mapboxAccount = 'mapbox';
+const mapboxStyle = 'light-v10';
+const mapboxToken = 'pk.eyJ1IjoieWVzZW5pYW8iLCJhIjoiY2tlZjAyM3p5MDNnMjJycW85bmpjenFkOCJ9.TDYe7XRNP8CnAto0kLA5zA'
+L.tileLayer(`https://api.mapbox.com/styles/v1/${mapboxAccount}/${mapboxStyle}/tiles/256/{z}/{x}/{y}@2x?access_token=${mapboxToken}`, {
+    maxZoom: 19,
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
 }).addTo(schoolMap);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       maxZoom: 19,
+//       attribution: '© OpenStreetMap',
+// }).addTo(schoolMap);
 
 showSchoolsOnMap(schools, schoolMap);
 
