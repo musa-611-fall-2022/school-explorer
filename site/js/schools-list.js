5506e9e3f00004d2ca5a27e4b72f3f138f0dc4d7
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-10 10:29:15 -0400

    I tried making a new schoolList variable did not work.

--- site/js/schools-list.js
+++ site/js/schools-list.js
@@ -1,18 +1,38 @@
 import { htmlToElement } from './template-tools.js';
 
-function showSchoolsInList(schoolsToShow, schoolList) {
+function makeSchoolFeatureList(school) {
+    return{
+        'type': 'Feature',
+        'id': school["sdp_id"],
+        'properties':{
+            "school_name": school["name"],
+            "Email": school["FACE Liason Email"],
+            "Phone Number": school["FACE Liasion Phone Number"],
+            "Admission type": school["Admission Type"],
+            "Grades Served": school["Current Grade Span Served"],
+        },
+    };
+}
+
+function showSchoolsInList(schoolsToShowList, schoolList) {
     console.log(schoolList);
     schoolList.innerHTML = '';
 
-    for (const school of schoolsToShow) {
+    const schoolFeatureCollectionList = {
+        "features": schoolsToShowList.map(makeSchoolFeatureList),
+    };
+
+
+    for (const school of schoolsToShowList) {
         const html = `
             <li class="school-list-item">${school['name']}</li>
         `;
         const li = htmlToElement(html);
-        schoolList.appendChild(li);
+        console.log("Hi Jingyi I put console.log before appendList")
+        schoolList.append(li);
     }
 }
 
 export {
     showSchoolsInList,
-}
+};
