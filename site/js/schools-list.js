37ae83869fbb553dfba9abb8436f359bf2ff0bf9
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-10 10:55:41 -0400

    About to mess with the CSS and HTML

--- site/js/schools-list.js
+++ site/js/schools-list.js
@@ -1,20 +1,14 @@
 import { htmlToElement } from './template-tools.js';
 
 function showSchoolsInList(schoolsToShow, schoolList) {
-    console.log(schoolList);
     schoolList.innerHTML = '';
-
-    const schoolFeatureCollectionList = {
-        "features": schoolsToShowList.map(makeSchoolFeatureList),
-    };
-
-
-    for (const school of schoolsToShowList) {
+    
+    for(const school of schoolsToShow) {
         const html = `
-            <li class="school-list-item">${school['name']}</li>
+        <li class="school-list-item">${school['name']}</li>
         `;
         const li = htmlToElement(html);
-        schoolList.appendChild(li);
+        schoolList.append(li);
     }
 }
 
