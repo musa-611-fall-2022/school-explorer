f960ee7726cc68ed3a949cb91944fbd6c13ffdeb
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-11 13:12:40 -0400

    Fixed things and linted

--- site/js/schools-list.js
+++ site/js/schools-list.js
@@ -2,14 +2,13 @@ import { htmlToElement } from './template-tools.js';
 
 function showSchoolsInList(schoolsToShow, schoolList) {
     schoolList.innerHTML = '';
-    
-    console.log("Hi Mjumbe :D")
+    console.log("Hi Mjumbe :D");
     for(const school of schoolsToShow) {
         const html = `
         <li class="school-list-item">${school['name']}</li>
         `;
         const li = htmlToElement(html);
-        console.log(schoolList)
+        console.log(schoolList);
         schoolList.append(li);
     }
 }
