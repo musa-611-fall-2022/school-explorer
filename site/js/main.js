2d263e37d6bb7f78c9351797f14f38b7fd11ac9e
Sofia <sfasull1@binghamton.edu>
2022-10-12 13:55:46 -0400

    got school points

--- site/js/main.js
+++ site/js/main.js
@@ -7,8 +7,39 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap',
 }).addTo(schoolMap);
 
+//Phila outline? do we need that?\
+
+function makeSchoolFeature(school){
+    return {
+    "type": "Feature",
+    "id": school['sdp_id'],
+    "properties": {
+      "school_name": school['name'],
+      "school_id": school['sdp_id'],
+      "school_sortname": school['sort_name'],
+    },
+    "geometry": school['geom'],
+  };
+}
+
+const schoolFeatureCollection = {
+  "type": "FeatureCollection",
+  "features": schools.map(makeSchoolFeature),
+};
+
+L.geoJSON(schoolFeatureCollection, {
+  pointToLayer: (geoJdonPoint, latlng) => L.circleMarker(latlng),
+  style: {
+    stroke: false,
+    fillOpacity: 1,
+    radius: 3,
+    color: "#ff7800"
+  }
+}).addTo(schoolMap);
 
 
 // Expose variables to the global scope
 window.schools = schools;
-window.mapview=schoolMap
+window.mapview = schoolMap;
+// might add a window.phila = phila; for an outline map
+window.makeSchoolFeature = makeSchoolFeature;
