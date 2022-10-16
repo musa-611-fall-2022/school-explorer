56561e461893a2723acb7466be352f79b8711250
zoenyoo <zoeyoo@berkeley.edu>
2022-10-16 18:12:53 -0400

    Update schools-list.js

--- site/js/schools-list.js
+++ site/js/schools-list.js
@@ -3,7 +3,7 @@ import { htmlToElement } from './template-tools.js';
 function showSchoolsInList(schoolsToShow, schoolList){
     schoolList.innerHTML = '';
     for (const schools of schoolsToShow){
-        const html = `<li class = "school-list-item">${schools['name','School Level']}</li>`;
+        const html = `<li class = "school-list-item">${schools['name', 'School Level']}</li>`;
         const li = htmlToElement(html);
         schoolList.append(li);
     }
