b80dce4a6e16875b81c49329ce825e943a3af61b
miaomiao612 <dddoctorr612@gmail.com>
2022-12-15 04:30:05 +0800

    update

--- site/js/template-tools.js
+++ site/js/template-tools.js
@@ -1,8 +1,8 @@
 /*
  * @Author: miaomiao612 dddoctorr612@gmail.com
- * @Date: 2022-10-15 22:49:43
+ * @Date: 2022-12-15 04:26:59
  * @LastEditors: miaomiao612 dddoctorr612@gmail.com
- * @LastEditTime: 2022-10-16 04:32:50
+ * @LastEditTime: 2022-12-15 04:28:45
  * @FilePath: \school-explorer\site\js\template-tools.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,7 @@ for more information: https://stackoverflow.com/a/35385518/123776
  * @param {String} HTML representing a single element
  * @return {Element}
  */
-function htmlToElement(html) {
+ function htmlToElement(html) {
   const template = document.createElement('template');
   const trimmedHtml = html.trim(); // Never return a text node of whitespace as the result
   template.innerHTML = trimmedHtml;
