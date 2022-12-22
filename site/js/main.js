6a6306ddcf1981ebd5a45c02f72021af71455851
Sofia <sfasull1@binghamton.edu>
2022-12-22 16:39:23 -0500

    filters overlap

--- site/js/main.js
+++ site/js/main.js
@@ -14,45 +14,45 @@ const schoolList = document.getElementById('#school-list');
 // these are all the unique school types ['High', 'Middle', 'Elementary-Middle', 'Elementary', 'Middle-High', 'Transition/Overage School', 'Elementary-Middle-High', 'Elementary-High']
 
 function getFilteredSchools(){
-
-};
-
-for (const checkbox of schoolCheckboxes){
-    checkbox.addEventListener('change', (evt) => {
-        if (evt.target.checked){ //evt.target is same as checkbox
-            console.log('you clicked on the checkbox ' + checkbox.value);
-            if (checkbox.value == "Transition/Overage School"){
-                const filteredSchools = schools.filter(
-                    school => (school['School Level']!="High" && school['School Level']!="Middle" && school['School Level']!="Elementary")
-                );
-                const black = "#000000";
-                showSchoolsOnMap(filteredSchools,schoolMap, black);
-            } else {
-                const filteredSchools = schools.filter(
-                        school => (school['School Level']==checkbox.value) //this excludes all the transition schools except Transition/Overage
-                );
-                const black = "#000000";
-                showSchoolsOnMap(filteredSchools,schoolMap, black);
-            }
-            console.log(evt.target);
-        } else {
-            showSchoolsOnMap(schools,schoolMap, orange);
-            console.log('you unclicked the checkbox ' + checkbox.value)
-        }
-        
-    })
-};
-
-
-schoolNameFilter.addEventListener('input',() =>{
+    let filteredSchools = schools;
+    //filter based on school name
     const text = schoolNameFilter.value;
-    const filteredSchools = schools.filter(function (school){
+    filteredSchools = filteredSchools.filter(function (school){
         const name = school['name'].toLowerCase();
         const hasText = name.includes(text);
         return hasText; //this is the true/false filter
     });
-    showSchoolsOnMap(filteredSchools,schoolMap,orange)
-})
+    //filter based on school type checkboxes   
+    for (const checkbox of schoolCheckboxes){
+        if (checkbox.checked){
+            if (checkbox.value == "Transition/Overage School"){
+                filteredSchools = filteredSchools.filter(
+                    school => (school['School Level']!="High" && school['School Level']!="Middle" && school['School Level']!="Elementary"));
+            } else {
+                filteredSchools = filteredSchools.filter(
+                    school => (school['School Level']==checkbox.value)); //this excludes all the transition schools except Transition/Overage
+            }
+        }
+    }
+    return filteredSchools;  
+};
+
+
+for (const checkbox of schoolCheckboxes){
+    checkbox.addEventListener('change', (evt) => {
+        const filteredSchools = getFilteredSchools();
+        const black = "#000000";
+        showSchoolsOnMap(filteredSchools,schoolMap, black);
+    });
+}
+
+
+
+schoolNameFilter.addEventListener('input',() =>{
+    const filteredSchools = getFilteredSchools();
+    const black = "#000000";
+    showSchoolsOnMap(filteredSchools,schoolMap, black);
+});
 
 
 
@@ -61,3 +61,25 @@ window.schools = schools; // was just being used to check the data properties
 window.mapview = schoolMap;
 // might add a window.phila = phila; for an outline map
 window.makeSchoolFeature = makeSchoolFeature;
+
+//checkbox.addEventListener('change', (evt) => {
+//    if (evt.target.checked){ //evt.target is same as checkbox
+//        console.log('you clicked on the checkbox ' + checkbox.value);
+//        if (checkbox.value == "Transition/Overage School"){
+//            const filteredSchools = schools.filter(
+//                school => (school['School Level']!="High" && school['School Level']!="Middle" && school['School Level']!="Elementary")
+//            );
+//            const black = "#000000";
+//            showSchoolsOnMap(filteredSchools,schoolMap, black);
+//        } else {
+//            const filteredSchools = schools.filter(
+//                    school => (school['School Level']==checkbox.value) //this excludes all the transition schools except Transition/Overage
+//            );
+//            const black = "#000000";
+//            showSchoolsOnMap(filteredSchools,schoolMap, black);
+//        }
+//        console.log(evt.target);
+//    } else {
+//        showSchoolsOnMap(schools,schoolMap, orange);
+//        console.log('you unclicked the checkbox ' + checkbox.value)
+//    })
